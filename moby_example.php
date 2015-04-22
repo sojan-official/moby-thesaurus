bde32b3099bb0f9e109fb60cbf2ae98c7bd8adce
sojan v jose <sojan@pepalo.com>
2015-04-23 00:02:52 +0530

    added readme and the sql data dump

--- moby_example.php
+++ moby_example.php
@@ -4,14 +4,14 @@ echo "<h1>Demonstrating MobyThesaurus Class</h1>";
 
 echo "<h3>Getting Synonyms for 'medications' (MobyThesaurus::GetSynonyms)</h3>";
 $start_time = microtime(true);
-$synonyms = MobyThesaurus::GetSynonyms("medications");
-var_export($synonyms);
+$synonyms = MobyThesaurus::GetSynonyms("medication");
+var_dump($synonyms);
 echo "<br><br>Processing Time: " . (microtime(true) - $start_time) . "<br>";
 
 echo "<br><h3>Getting Part of Speech for 'feel' (MobyThesaurus::GetPartsOfSpeech)</h3>";
 $start_time = microtime(true);
-$pos = MobyThesaurus::GetPartsOfSpeech("feel");
-var_export($pos);
+$pos = MobyThesaurus::GetPartsOfSpeech("cat");
+var_dump($pos);
 echo "<br><br>Processing Time: " . (microtime(true) - $start_time) . "<br>";
 
 ?>
